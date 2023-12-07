615354b96912496290bed9b597337e2548637336
Biagio333 <cristiano.piacenti@gmail.com>
2023-12-07 17:14:13 +0100

    Initial commit

---
+++ app/src/main/java/com/croen/dispenser_app_v20/FirstFragment.java
@@ -0,0 +1,64 @@
+package com.croen.dispenser_app_v20;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.croen.dispenser_app_v20.databinding.FragmentFirstBinding;
+
+import android.webkit.WebSettings;
+
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private WebView webView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+
+        webView = rootView.findViewById(R.id.webView_1);
+        // Abilita l'esecuzione di JavaScript (se necessario)
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Carica l'URL desiderato
+        webView.loadUrl("https://croen.org");
+
+        return rootView;
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
